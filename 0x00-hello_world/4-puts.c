affdb335d08454653b4c7542a33e035dc168b6e7
Florence-wangui <wanguif018@gmail.com>
2022-03-10 10:12:14 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
-
+#include <studio.h>
 
 /**
 
  * main - Entry point
 
- * Return: Always 1 (Success)
+ *
+
+ * Return: Always 0 (Success)
 
  */
 
@@ -16,10 +14,8 @@ int main(void)
 
 {
 
-	write(STDOUT_FILENO, "and that piece of art is useful
+	puts("\"Programming is like building a multilingual puzzle");
 
-			\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
+	return (0);
 
 }
