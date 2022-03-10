e52ac555ebddaffe64da9ab0ed7788b1263725ef
Florence-wangui <wanguif018@gmail.com>
2022-03-10 10:20:13 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main (void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
