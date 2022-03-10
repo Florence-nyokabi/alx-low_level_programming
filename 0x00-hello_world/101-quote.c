ccb159e44dde2701c5bbadc9dd65cd346b88d540
Florence-wangui <wanguif018@gmail.com>
2022-03-10 13:34:49 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
 * main - prints exactly "and that piece of art is useful"
-* - Dora Korpar,  2015-10-19"
+* - Dora Korpar, 2015-10-19"
 * followed by a new line, to the standard error.
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
