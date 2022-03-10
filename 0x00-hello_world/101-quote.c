ff3301074d149326f0547a06638b8db8576bcd66
Florence-wangui <wanguif018@gmail.com>
2022-03-10 12:28:45 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
