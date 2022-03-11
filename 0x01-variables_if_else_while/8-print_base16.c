2f912d250269f010f22ce3367d200b9b3548e433
Florence-wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-03-11 22:22:26 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return:0
- */ 
+* main - Entry point
+* Return:0
+*/ 
 int main(void)
 {
 	int i, y;
+	
 	for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
