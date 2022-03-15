6d36ad360c97c036606c63d3c8971b96b8397594
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-03-15 16:10:08 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,17 @@
 #include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+char alphabet [26] = "abcdefghijklmnopqrstuvwxyz";
+int 1; 
+for (1 = 0; i < 26; i++)
+{
+  _putchar(alphabet[1]);
+}
+ _putchar('\n');
+}
 
