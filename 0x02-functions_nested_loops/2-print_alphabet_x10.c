71ca29353f8bc2e0d0db0d6b29c43b727fb4dc14
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-03-15 21:02:15 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 
 /**
  * print_alphabet_x10 - prints alphabet 10 times
@@ -7,18 +7,19 @@
 
 void print_alphabet_x10(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i, j;
-	i = 0;
+	char c;
+	int i = 0;
+
 	while (i < 10)
 	{
-	j = 0;
-	while (j < 26)
-	{
-	_putchar(alphabet[j]);
-	j++;
-	}
-	_putchar('\n');
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
 		i++;
 	}
+
 }
