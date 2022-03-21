90319bdb0a7840a9e30c718a8ad1e8dc630884b4
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-03-21 17:50:22 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef _main_h_
 #define _main_h_
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,5 +11,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 #endif
