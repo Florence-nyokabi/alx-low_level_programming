3d004ccd2098a80fba5e2e4b7d83f5473e973f0f
Florence-wangui <wanguif018@gmail.com>
2022-03-28 08:47:39 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any set of bytes
+ *@s: source string
+ *@accept: searching string
+ *
+ * Return: new string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if(*(s + i) == *(accept + j))
+			{
+				break;
+			}
+		}
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
