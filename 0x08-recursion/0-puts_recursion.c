6cedb5bf41a6ca182910995d5a30334e93e77bf5
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-03-30 16:52:10 +0300

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- *
- * Return: On success 1.
- * On error, -1.
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
  */
+
+
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	{
-		_putchar('\n');
-		return;
-	}
+
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+else
+{
 	_putchar(*s);
-	_put_recursion (s+1);
+	_puts_recursion(s + 1);
+}
+
 }
