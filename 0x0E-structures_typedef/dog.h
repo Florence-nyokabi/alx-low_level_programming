0a10e8f3c74d688e7c78d4febe8c45fc5c4255bf
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-04-11 15:06:26 +0300

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* _dog_h_ */
