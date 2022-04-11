ac297aaca41f73bc8eee3a89b4874e5c39181813
Florence-wangui <wanguif018@gmail.com>
2022-04-11 06:47:03 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from, followed by a new line.
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
