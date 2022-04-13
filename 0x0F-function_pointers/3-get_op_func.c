de78cb27c264fab9f9534709e1aba49e4119a0a8
Florence-wangui <wanguif018@gmail.com>
2022-04-13 08:57:55 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - a  function that selects the correct function to perform the
+ * operation asked by the user. You’re not allowed to declare any other
+ * function
+ * @s: operator passed as argument to the program
+ *
+ * Return: a pointer to the function that corresponds to the operator given as a
+ * parameter
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	int i;
+	i = 0;
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+
+	     i++;
+	}
+
+	return (0);
+
+}
