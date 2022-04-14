d72ba33208a1cac2812d6c6e54d5c2d783b9fcfa
Florence-wangui <wanguif018@gmail.com>
2022-04-14 01:28:42 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _variadic_functions_h
+#define _variadic_functions_h
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
