04b4f7aff281b601ed58ffe57f67c84a5f45a317
Florence-wangui <wanguif018@gmail.com>
2022-04-21 04:06:02 -0700

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,13 +11,9 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
-	}
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
 }
