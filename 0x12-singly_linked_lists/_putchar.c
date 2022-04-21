d4329d235e4b924d42b3452c042190205ac19e1a
Florence-wangui <wanguif018@gmail.com>
2022-04-21 01:54:48 -0700

    putchar

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_putchar - prints a character
+ * @c: character to be printed
+ *
+ * Return: On success 1, on fail -1
+ */
+
+int putchar (char c)
+{
+	return (write 1, &c, 1));
+}
