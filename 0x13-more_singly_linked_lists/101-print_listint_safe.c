910d720cd36b02a52eac0f11b7fdb1eae00dd1c6
Florence-wangui <wanguif018@gmail.com>
2022-04-26 00:54:01 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,10 +16,8 @@ size_t print_listint_safe(const listint_t *head)
 		diff = head - head->next;
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-
 		if (diff > 0)
 			head = head->next;
-		
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
