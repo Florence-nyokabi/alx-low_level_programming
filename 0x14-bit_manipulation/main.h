f5741d895963a1d74dfdb5ef97d0763370548f36
Florence-wangui <wanguif018@gmail.com>
2022-04-28 04:17:52 -0700

    header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
