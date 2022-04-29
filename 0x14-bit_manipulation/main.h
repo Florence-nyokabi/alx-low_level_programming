f657eb6a030b912ece39d19cf6d6005500723242
Florence-wangui <wanguif018@gmail.com>
2022-04-29 07:01:44 -0700

    correct

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int putchar(int char);
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
