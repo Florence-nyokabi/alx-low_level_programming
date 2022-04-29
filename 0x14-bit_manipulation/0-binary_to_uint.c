61ddae0b60ef7631b125b1038648fa408aa81452
Florence-wangui <wanguif018@gmail.com>
2022-04-29 01:29:49 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,21 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int dec_val = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
+	while (b[len])
+		len++;
 
-	return (dec_val);
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len -1] != '1')
+			return (0);
+
+		if (b[len - 1] == '1')
+			result +=base;
+		base *= 2;
+		len--;
+	}
+	return (result);
 }
