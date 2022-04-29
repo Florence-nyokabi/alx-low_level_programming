f8af8c100411833e1a15ccd8c1bf197d50e6a319
Florence-wangui <wanguif018@gmail.com>
2022-04-29 07:13:38 -0700

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,14 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int max = 0x01;
+
+	max <<= index;
+	if (max == 0)
 		return (-1);
-	return ((n >> index) & 1);
+
+	if ((n & max))
+		return (1);
+	else
+		return (0);
 }
