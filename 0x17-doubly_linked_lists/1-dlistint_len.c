7e284056a2a916527cfb18eed198e5b785fe714f
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-09 13:10:28 +0300

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements
- * @h: pointer
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
  *
  * Return: number of nodes
  */
@@ -11,8 +11,6 @@ size_t dlistint_len(const dlistint_t *h)
 	size_t i;
 
 	for (i = 0; h != NULL; i++)
-	{	
-		h = h ->next;
-	}
+		h = h->next;
 	return (i);
 }
