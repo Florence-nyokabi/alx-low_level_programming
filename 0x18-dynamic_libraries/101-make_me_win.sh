9c5cdd150020d74d4ac255a36f871c622d0cbcec
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-13 20:41:08 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Florence-wangui/alx-low_level_programming/edit/master/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/101-make_me_win.sh
+wget -O /tmp/win.so https://github.com/Florence-wangui/alx-low_level_programming/blob/master/0x18-dynamic_libraries/wins.so
+export LD_PRELOAD=/tmp/wins.so
