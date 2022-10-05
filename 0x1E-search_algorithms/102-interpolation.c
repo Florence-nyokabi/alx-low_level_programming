ce2089bc5ddc04fa5cd400087e9059fdcdcaf662
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-10-05 15:01:59 +0300

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,7 +10,7 @@
   * Return: If the value is not present or the array is NULL, -1.
   *         Otherwise, the first index where the value is located.
   *
-  * Description: Prints a value every time it is compared in the array..
+  * Description: Prints a value every time it is compared in the array.
   */
 int interpolation_search(int *array, size_t size, int value)
 {
